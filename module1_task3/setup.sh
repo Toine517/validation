7b7fca36b8919413253e78fb92320b04c5d4e9ad
Antoine Pesme <antoine@debian-apesme>
2021-09-09 15:43:25 +0200

    module1_task3

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+if ! command -v hugo &> /dev/null
+then
+    echo "hugo is not present"
+fi
+
+if ! command -v make &> /dev/null
+then
+    echo "make is not present"
+fi
+
 make build
 
