9fcb4b6f99efda8d2b945f27351fac2db190ca37
Antoine Pesme <antoine@debian-apesme>
2021-09-10 09:22:18 +0200

    module1_task3

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -2,16 +2,21 @@
 echo "testing for is hugo PRESENT"
 if ! command -v hugo &> /dev/null
 then
-    echo "FAILS"
-    exit
+    echo FAILS
+    echo installing hugo
+    apt-get update
+    apt-get install hugo
 else
-    echo "PASS"
+    echo PASS
 fi
 
 echo "testing for is make PRESENT"
 if ! command -v make &> /dev/null
 then
-    echo "FAILS"
+    echo FAILS
+    echo installing make
+    apt-get update
+    apt-get install make
 else
     echo "PASS"
 fi
