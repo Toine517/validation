a9146847850e989160a5fba5319baa275c4bae6a
Antoine Pesme <antoine@debian-apesme>
2021-09-10 09:51:45 +0200

    module1_task4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -4,8 +4,9 @@ if ! command -v hugo &> /dev/null
 then
     echo FAILS
     echo installing hugo
-    apt-get update
-    apt-get install hugo
+    apt-get -qq update > NULL 2>&1
+    apt-get -y -qq install hugo > NULL 2>&1
+    echo hugo is installed
 else
     echo PASS
 fi
@@ -15,11 +16,16 @@ if ! command -v make &> /dev/null
 then
     echo FAILS
     echo installing make
-    apt-get update
-    apt-get install make
+    apt-get -qq update > NULL 2>&1
+    apt-get -y -qq install make > NULL 2>&1
+    echo make is installed
 else
     echo "PASS"
 fi
+
+apt-get -y -qq install wget > NULL 2>&1
+wget https://github.com/gohugoio/hugo/releases/download/v0.76.5/hugo_extended_0.76.5_Linux-64bit.deb > NULL 2>&1
+dpkg -i hugo_extended_0.76.5_Linux-64bit.deb > NULL 2>&1
 
 make build 2>&1
 
